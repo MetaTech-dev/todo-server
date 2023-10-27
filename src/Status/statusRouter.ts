f7cade9bd150fa6a2f9083b4ddd9fa5084ba49ce
Zaq <zaqreynolds@gmail.com>
2023-10-26 20:54:26 -0700

    Add getOne method to statusRouter
    This commit adds the getOne method to the statusRouter, allowing users to retrieve a specific status by its ID.

--- src/Status/statusRouter.ts
+++ src/Status/statusRouter.ts
@@ -3,11 +3,12 @@ import StatusController from "./statusController";
 
 const router = express.Router();
 
-const { list, create, update, remove } = new StatusController();
+const { list, create, update, remove, getOne } = new StatusController();
 
 router.get("/", list);
 router.post("/", create);
 router.put("/", update);
 router.delete("/:id", remove);
+router.get("/:id", getOne);
 
 export default router;
