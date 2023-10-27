8f44f80ca8198bff9fffa62e5017e8127c07bec9
Zaq <zaqreynolds@gmail.com>
2023-10-26 18:15:29 -0700

    Refactor status update logic and handle position changes
    - Refactored the `update` method in the `StatusService` class to handle updating the position of statuses when the position of a status is changed.
    - Added logic to find the existing status to be updated and throw an error if it is not found.
    - Implemented logic to get the starting position and new position of the updated status.
    - If the new position is different from the starting position, update the positions of all impacted statuses accordingly.
    - Added error handling for deleting a status that is in use.
    
    This commit improves the functionality and reliability of updating statuses by properly handling changes in their positions.

--- src/Status/statusService.ts
+++ src/Status/statusService.ts
@@ -39,18 +39,94 @@ export default class StatusService {
     }
   };
 
-  // TODO: handle errors for these
-
   static update = async (
     status: UpdateStatusDTO
   ): Promise<Status | Prisma.PrismaClientKnownRequestError> => {
     try {
+      // Find the status to be updated
+      const existingStatus = await prisma.status.findUnique({
+        where: {
+          id: status.id,
+        },
+      });
+
+      if (!existingStatus) {
+        throw new Error("Status not found");
+      }
+
+      // Get the starting position
+      const startingPosition = existingStatus.position;
+
       const updatedStatus = await prisma.status.update({
         where: {
           id: status.id,
         },
         data: status,
       });
+
+      // Get the new position
+
+      const newPosition = updatedStatus.position;
+
+      // check if the position has changed
+      if (newPosition !== startingPosition) {
+        // if it has, update the position of all impacted statuses
+        if (newPosition > startingPosition) {
+          await prisma.status.updateMany({
+            where: {
+              AND: [
+                {
+                  position: {
+                    gt: startingPosition,
+                  },
+                },
+                {
+                  position: {
+                    lte: newPosition,
+                  },
+                },
+                {
+                  NOT: {
+                    id: status.id, // Exclude the updatedStatus
+                  },
+                },
+              ],
+            },
+            data: {
+              position: {
+                decrement: 1,
+              },
+            },
+          });
+        } else if (newPosition < startingPosition) {
+          await prisma.status.updateMany({
+            where: {
+              AND: [
+                {
+                  position: {
+                    lt: startingPosition,
+                  },
+                },
+                {
+                  position: {
+                    gte: newPosition,
+                  },
+                },
+                {
+                  NOT: {
+                    id: status.id, // Exclude the updatedStatus
+                  },
+                },
+              ],
+            },
+            data: {
+              position: {
+                increment: 1,
+              },
+            },
+          });
+        }
+      }
 
       return updatedStatus;
     } catch (err) {
@@ -99,6 +175,7 @@ export default class StatusService {
       return true;
     } catch (err) {
       if (err instanceof Prisma.PrismaClientKnownRequestError) {
+        // if status is in use, throw error
         if (err.code === "P2003") {
           console.log("Unable to delete status, it is in use", err.code);
 
