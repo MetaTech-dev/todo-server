935c64a18bd94b50ea362c0f2e97f202bb50a06f
Zaq <zaqreynolds@gmail.com>
2023-11-13 16:26:15 -0800

    feat: added morgan and Cors, now able to have client read data from api

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
-import express, {Express, Request, Response} from 'express';
-import router from './router';
+import express, { Express, Request, Response } from "express";
+import router from "./router";
+import morgan from "morgan";
+import cors from "cors";
 
 const app: Express = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
 
-app.get('/', (_req: Request, res: Response)=>{
-    res.send('Hello World!');
+app.get("/", (_req: Request, res: Response) => {
+  res.send("Hello World!");
 });
 
-app.use(router)
+app.use(router);
 
-app.listen(port, ()=> {
-console.log(`[Server] running at https://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`[Server] running at https://localhost:${port}`);
 });
