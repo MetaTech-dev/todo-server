c1eec6c4651fb02a964d4d4bc3b9ce1e7750c027
Zaq Reynolds <zaqreynolds@gmail.com>
2023-12-15 17:42:51 -0800

    feat: Add permission error handler middleware
    - Added `permissionErrorHandler` middleware to handle insufficient scope errors.
    - The middleware checks if the error is an instance of `InsufficientScopeError` and returns a JSON response with a "Permission denied" message.
    - The new middleware is imported and used in the main application file.

--- src/ToDo/toDoRouter.ts
+++ src/ToDo/toDoRouter.ts
@@ -1,20 +1,26 @@
 import express from "express";
 import ToDoController from "./ToDoController";
 import checkAuth from "../middleware/checkAuth";
+import checkPermissions from "../middleware/checkPermissions";
 
 const router = express.Router();
 
 const { list, create, update, updateAll, remove, getOne } =
   new ToDoController();
 
-router.get("/", checkAuth, list);
-router.post("/", checkAuth, create);
-router.put("/:id", checkAuth, update);
-router.put("/", checkAuth, updateAll);
-router.delete("/", checkAuth, (_req, res) => {
-  return res.status(400).json({ message: "ID is required" });
-});
-router.delete("/:id", checkAuth, remove);
-router.get("/:id", checkAuth, getOne);
+router.get("/", checkAuth, checkPermissions(["read:toDo"]), list);
+router.post("/", checkAuth, checkPermissions(["create:toDo"]), create);
+router.put("/:id", checkAuth, checkPermissions(["update:toDo"]), update);
+router.put("/", checkAuth, checkPermissions(["update:toDo"]), updateAll);
+router.delete(
+  "/",
+  checkAuth,
+  checkPermissions(["delete:toDo"]),
+  (_req, res) => {
+    return res.status(400).json({ message: "ID is required" });
+  }
+);
+router.delete("/:id", checkAuth, checkPermissions(["delete:toDo"]), remove);
+router.get("/:id", checkAuth, checkPermissions(["read:toDo"]), getOne);
 
 export default router;
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from "express";
 import router from "./router";
 import morgan from "morgan";
 import cors from "cors";
+import { permissionErrorHandler } from "./middleware/permissionErrorHandler";
 
 const app: Express = express();
 const port = 3000;
@@ -15,6 +16,7 @@ app.get("/", (_req: Request, res: Response) => {
 });
 
 app.use(router);
+app.use(permissionErrorHandler);
 
 app.listen(port, () => {
   console.log(`[Server] running at https://localhost:${port}`);
