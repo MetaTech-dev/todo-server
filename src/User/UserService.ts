52342fa9bc3b2aa000a0c3e5a5315e12bcf05c62
Zaq Reynolds <zaqreynolds@gmail.com>
2023-12-18 15:33:15 -0800

    feat(UserService): Update user management and role assignment
    updated ManagementClient's clientID and clientSecret

--- src/User/UserService.ts
+++ src/User/UserService.ts
@@ -1,60 +1,60 @@
-import { ManagementClient, UserUpdate } from 'auth0';
+import { ManagementClient, UserUpdate } from "auth0";
 
 const management = new ManagementClient({
-  domain: 'https://metatech-todo-dev.us.auth0.com/api/v2/',
-  clientId: '87yrCCRUTtwjvJ8flg5ZhzzB4kI6JMva',
-  clientSecret: 'uVB35ukoTqdjP2gQvCIr24Td8jQbQYw3Hb2oTGzPMGqqcy23SnYmAxvgSrAIToqz',
+  domain: "https://metatech-todo-dev.us.auth0.com/api/v2/",
+  clientId: "mg6cH2lq8jIEpEzIqgdpP3G2pethhWJq",
+  clientSecret:
+    "sTyLLH7B-HwPnYwwJMlQk0yy-E25wnuJLlYUIuvxvLWfkT_Y_5N8HhR6OTBWF9r9",
 });
 
 export default class UserService {
-    // note: create is handled by Auth0 when the user signs up on the frontend so we don't need to create a user here
+  // note: create is handled by Auth0 when the user signs up on the frontend so we don't need to create a user here
 
-    update = async (id: string, body: UserUpdate) => {
-        await management.users.update({ id }, body);
+  update = async (id: string, body: UserUpdate) => {
+    await management.users.update({ id }, body);
+  };
+
+  getOne = async (id: string) => {
+    // get user without roles
+    const userResponse = await management.users.get({ id });
+
+    // get roles for user
+    let rolesResponse = await management.users.getRoles({ id });
+
+    // if the user doesn't have any roles, assign the default role
+    if (!rolesResponse.data.length) {
+      await this.assignRoles(id, ["member"]);
+      rolesResponse = await management.users.getRoles({ id });
     }
 
-    getOne = async (id: string) => {
-        // get user without roles
-        const userResponse = await management.users.get({ id });
+    // add roles to user object
+    const user = {
+      ...userResponse.data,
+      roles: rolesResponse.data,
+    };
 
-        // get roles for user
-        let rolesResponse = await management.users.getRoles({ id });
+    return user;
+  };
 
-        // if the user doesn't have any roles, assign the default role
-        if (!rolesResponse.data.length) {
-            await this.assignRoles(id, ['member']);
-            rolesResponse = await management.users.getRoles({ id });
-        }
+  list = async () => {
+    // get the list of users without roles
+    const usersResponse = await management.users.getAll();
 
-        // add roles to user object
-        const user = {
-           ...userResponse.data,
-              roles: rolesResponse.data,
+    // loop through users, get roles for each user, and add roles to user object
+    const users = await Promise.all(
+      usersResponse.data.map(async (user) => {
+        const rolesResponse = await management.users.getRoles({ id: user.id });
+        return {
+          ...user,
+          roles: rolesResponse.data,
         };
+      })
+    );
 
-        return user;
-        }
-    
-    list = async () => {
-        // get the list of users without roles
-        const usersResponse = await management.users.getAll();
+    return users;
+  };
 
-        // loop through users, get roles for each user, and add roles to user object
-        const users = await Promise.all(
-            usersResponse.data.map(async (user) => {
-                const rolesResponse = await management.users.getRoles({ id: user.id });
-                return {
-                    ...user,
-                    roles: rolesResponse.data,
-                };
-            })
-        );
-
-        return users;
-    }
-
-    assignRoles = async (id: string, roles: string[]) => {
-        await management.users.assignRoles({ id }, { roles });
-    }
+  assignRoles = async (id: string, roles: string[]) => {
+    await management.users.assignRoles({ id }, { roles });
+  };
 }
-
