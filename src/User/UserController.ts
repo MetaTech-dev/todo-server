5cd3f2ee805d62721081cee641dcc4d01feb649e
Zaq Reynolds <zaqreynolds@gmail.com>
2023-12-19 14:33:27 -0800

    feat(UserController): add CRUD operations
    - Added `list` method to retrieve a list of users
    - Added `getOne` method to retrieve a single user by ID
    - Added `update` method to update a user's information
    - Added `assignRoles` method to assign roles to a user

---
+++ src/User/UserController.ts
@@ -0,0 +1,66 @@
+import { Request, Response } from "express";
+import BaseController from "../BaseController";
+import UserService from "./UserService";
+
+export default class UserController extends BaseController {
+  list = async (_req: Request, res: Response) => {
+    try {
+      const users = await UserService.list();
+      return this.success(res, users);
+    } catch (err) {
+      return this.badRequest(res, err);
+    }
+  };
+
+  getOne = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+      if (!id) {
+        return this.badRequest(res, { message: "ID is required" });
+      }
+      const user = await UserService.getOne(id);
+
+      return this.success(res, user);
+    } catch (err) {
+      return this.badRequest(res, err);
+    }
+  };
+
+  update = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { body } = req.body;
+
+    try {
+      if (Object.keys(body).length === 0) {
+        return this.badRequest(res, { message: "request body is required" });
+      } else if (!id) {
+        return this.badRequest(res, { message: "ID is required" });
+      }
+
+      const updatedUser = await UserService.update(id, { body });
+
+      return this.created(res, updatedUser);
+    } catch (err) {
+      return this.badRequest(res, err);
+    }
+  };
+
+  assignRoles = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { roles } = req.body;
+
+    try {
+      if (!id) {
+        return this.badRequest(res, { message: "ID is required" });
+      } else if (!roles) {
+        return this.badRequest(res, { message: "Roles are required" });
+      }
+
+      const assignedUser = await UserService.assignRoles(id, roles);
+
+      return this.created(res, assignedUser);
+    } catch (err) {
+      return this.badRequest(res, err);
+    }
+  };
+}
