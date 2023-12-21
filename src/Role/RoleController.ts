11fb522568f204bfec5d8b1642f7f713f591e40c
Zaq Reynolds <zaqreynolds@gmail.com>
2023-12-20 18:51:18 -0800

    feat(RoleController): Remove console.log statements in list method
    - Removed unnecessary console.log statements in the list method of RoleController.

--- src/Role/RoleController.ts
+++ src/Role/RoleController.ts
@@ -5,9 +5,7 @@ import RoleService from "./RoleService";
 export default class RoleController extends BaseController {
   list = async (_req: Request, res: Response) => {
     try {
-      console.log("in controller");
       const roles = await RoleService.list();
-      console.log("roles in controller fuck", roles);
       return this.success(res, roles);
     } catch (err) {
       return this.badRequest(res, err);
