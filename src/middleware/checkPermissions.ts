43e89177139f8bb923195dea5b3aee491d473e71
Tony Stark <tony@riotracers.com>
2024-01-21 11:03:05 -0800

    fix(checkPermissions): temporarily hard code list of permissions on roles

--- src/middleware/checkPermissions.ts
+++ src/middleware/checkPermissions.ts
@@ -1,18 +1,51 @@
 import { RequireAuthProp } from "@clerk/clerk-sdk-node";
 import { Request, Response, NextFunction } from "express";
 
+const permissionsOnRoles = {
+  "org:admin": [
+    "org:create:status",
+    "org:create:todo",
+    "org:delete:status",
+    "org:delete:todo",
+    "org:read:status",
+    "org:read:todo",
+    "org:read:user",
+    "org:update:status",
+    "org:update:todo",
+    "org:update:user",
+    "org:update:userrole",
+  ],
+  "org:member": [
+    "org:read:status",
+    "org:read:todo",
+    "org:read:user",
+    "org:update:todo",
+    "org:update:user",
+  ],
+};
+
 const checkPermissions = (requiredPermissions: string[]) => {
   return async (
     req: RequireAuthProp<Request>,
     _res: Response,
     next: NextFunction
   ) => {
-    const orgId = req.query.orgId?.toString();
-    if (orgId) {
+    const reqOrgId = req.query.orgId?.toString();
+    // TODO: Fix this, waiting on clerk support
+    // @ts-ignore
+    const { orgId: authOrgId, orgRole: authOrgRole } = req.auth;
+    console.log(req.auth);
+
+    if (reqOrgId) {
+      if (reqOrgId !== authOrgId) {
+        return next(new Error("Unauthorized"));
+      }
+
       const hasPermissions = requiredPermissions.every((requiredPermission) =>
         // TODO: Fix this, waiting on clerk support
         // @ts-ignore
-        req.auth.orgPermissions?.includes(`org:${requiredPermission}`)
+        // req.auth.orgPermissions?.includes(`org:${requiredPermission}`)
+        permissionsOnRoles[authOrgRole].includes(`org:${requiredPermission}`)
       );
       if (!hasPermissions) {
         return next(new Error("Unauthorized"));
