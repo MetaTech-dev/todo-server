5ddf793097ee1a65a91d3270889df32e79728a94
Zaq Reynolds <zaqreynolds@gmail.com>
2024-01-24 14:55:21 -0800

    feat(RoleService): Comment out unused import
    - Commented out the import statement for `clerkClient` in `RoleService.ts` as it is currently not being used.
    
    fix(ToDoController): Add authorization check
    
    - Added an authorization check in the `getOne` method of `ToDoController.ts`.
    - The check ensures that only authorized users can access a specific ToDo item.
    - This change is pending support from Clerk.
    
    refactor(ToDoService): Remove unnecessary type annotation
    
    - Removed the unnecessary type annotation for the return value of the `getOne` method in `ToDoService.ts`.

--- src/Role/RoleService.ts
+++ src/Role/RoleService.ts
@@ -1,13 +1,13 @@
-import clerkClient from "@clerk/clerk-sdk-node";
+// import clerkClient from "@clerk/clerk-sdk-node";
 
 export default class RoleService {
   static list = async ({ orgId }: { orgId: string }) => {
     if (!orgId) {
       throw new Error("orgId is required");
     }
-    const organization = await clerkClient.organizations.getOrganization({
-      organizationId: orgId,
-    });
+    // const organization = await clerkClient.organizations.getOrganization({
+    //   organizationId: orgId,
+    // });
 
     // TODO: get the list of roles from Clerk, there is no SDK method for this yet
 
